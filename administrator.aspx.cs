e265a47044e2304938e121fcca0a7c200dd415f2
Afsal <afsalat9@gmail.com>
2022-12-03 00:14:03 +0530

    added new buttons and click layer showing function setted

--- administrator.aspx.cs
+++ administrator.aspx.cs
@@ -11,4 +11,36 @@ public partial class adminstrator : System.Web.UI.Page
     {
 
     }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 0;
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 1;
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 2;
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 3;
+    }
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 4;
+    }
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 5;
+    }
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 6;
+    }
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 7;
+    }
 }
